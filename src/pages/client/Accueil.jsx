ff00a4e3755e6b1a4e87a9fda7520203a0c9739b
fatima-ezzahraalouane <falouane38@gmail.com>
2025-04-16 19:47:49 +0100

    PV-11 PV-12 PV-13 PV-14 PV-15 PV-16 PV-66 PV-70 PV-86 PV-87 PV-88 PV-89 Ajout de l'importation de useEffect et useState dans le composant Accueil

--- src/pages/client/Accueil.jsx
+++ src/pages/client/Accueil.jsx
@@ -1,6 +1,5 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import { Link, useNavigate } from "react-router-dom";
-import { useEffect, useState } from "react";
 import axios from "axios";
 
 export default function Accueil() {
